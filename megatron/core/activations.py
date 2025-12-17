94bb859ab17eeea1b2d3c32e0b66d1116a78f325
Allen <allenhuangdd@gmail.com>
2025-12-17 15:30:30 +0100

    Update activations.py

--- megatron/core/activations.py
+++ megatron/core/activations.py
@@ -63,8 +63,8 @@ class XSSSLUPR(MegatronModule):
     def __init__(self, config=None, alpha_p_init=0.8, alpha_n_init=0.8, beta=0.5, eps=-1e-6, dtype=torch.bfloat16):
         super().__init__(config=config)
         self.config = config
-        self.alpha_p1 = nn.Parameter(torch.log(torch.exp(torch.tensor(0.8, dtype=dtype)) - 1.0).unsqueeze(0))
-        self.alpha_p2 = nn.Parameter(torch.log(torch.exp(torch.tensor(0.1, dtype=dtype)) - 1.0).unsqueeze(0))
+        self.alpha_p1 = nn.Parameter(torch.log(torch.exp(torch.tensor(0.5, dtype=dtype)) - 1.0).unsqueeze(0))
+        self.alpha_p2 = nn.Parameter(torch.log(torch.exp(torch.tensor(0.5, dtype=dtype)) - 1.0).unsqueeze(0))
         self.alpha_n = nn.Parameter(torch.log(torch.exp(torch.tensor(alpha_n_init - beta, dtype=dtype)) - 1.0).unsqueeze(0))
         self.beta = beta
         self.eps = torch.tensor(eps, dtype=torch.bfloat16, device='cuda')
